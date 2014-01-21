aca652cde4934cee2c92a562010e4f5b62d1e04b
Sigifredo Escobar Gómez <sigifredo.escobar@cabitech.co>
2014-01-21 11:08:26 -0500

    Intentamos guardar el video.
    Referencia: http://docs.opencv.org/doc/tutorials/highgui/video-write/video-write.html

--- src/fdetection.cpp
+++ src/fdetection.cpp
@@ -1,65 +1,100 @@
 
 // http://mymobilerobots.com/myblog/academic/tutorial-opencv-2-4-3-face-tracking-detection-using-vs-2010-c/
 
+// OpenCV
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+// std
+#include <cstdio>
 #include <iostream>
-#include <stdio.h>
 
-using namespace std;
-using namespace cv;
+// unix
+#include <signal.h>
+
+#define NAME        "/tmp/SS"
+
+bool saveVideo = true;
+
+void interruptHandler(int);
 
 int main(int argc, const char** argv)
 {
+    unsigned int i;
+    //setup image files used in the capture process
+    cv::Mat captureFrame;
+    cv::Mat grayscaleFrame;
+    bool faceDetected;
+
     //create the cascade classifier object used for the face detection
-    CascadeClassifier face_cascade;
+    cv::CascadeClassifier face_cascade;
     //use the haarcascade_frontalface_alt.xml library
     face_cascade.load("haarcascade_frontalface_alt.xml");
 
     //setup video capture device and link it to the first capture device
-    VideoCapture captureDevice;
-    captureDevice.open(0);
+    cv::VideoCapture captureDevice(0);
 
-    //setup image files used in the capture process
-    Mat captureFrame;
-    Mat grayscaleFrame;
+    int ex = static_cast<int>(captureDevice.get(CV_CAP_PROP_FOURCC));     // Get Codec Type- Int form
+    cv::Size S = cv::Size((int) captureDevice.get(CV_CAP_PROP_FRAME_WIDTH),    // Acquire input size
+                  (int) captureDevice.get(CV_CAP_PROP_FRAME_HEIGHT));
 
-    //create a window to present the results
-    namedWindow("outputCapture", 1);
+    // video writer
+    cv::VideoWriter outputVideo;
+    outputVideo.open(NAME, -1, captureDevice.get(CV_CAP_PROP_FPS), S, true);
 
-    //create a loop to capture and find faces
-    while(true)
+    if (captureDevice.isOpened() && outputVideo.isOpened()) // captureDevice.open(0);
     {
-        //capture a new image frame
-        captureDevice>>captureFrame;
+        //create a window to present the results
+        cv::namedWindow("outputCapture", 1);
 
-        //convert captured image to gray scale and equalize
-        cvtColor(captureFrame, grayscaleFrame, CV_BGR2GRAY);
-        equalizeHist(grayscaleFrame, grayscaleFrame);
+        signal(SIGINT, interruptHandler);
+        signal(SIGTERM, interruptHandler);
 
-        //create a vector array to store the face found
-        std::vector<Rect> faces;
-
-        //find faces and store them in the vector array
-        face_cascade.detectMultiScale(grayscaleFrame, faces, 1.1, 3, CV_HAAR_FIND_BIGGEST_OBJECT|CV_HAAR_SCALE_IMAGE, Size(30,30));
-
-        //draw a rectangle for all found faces in the vector array on the original image
-        for(int i = 0; i < faces.size(); i++)
+        //create a loop to capture and find faces
+        while (saveVideo)
         {
-            Point pt1(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
-            Point pt2(faces[i].x, faces[i].y);
+            faceDetected = false;
 
-            rectangle(captureFrame, pt1, pt2, cvScalar(0, 255, 0, 0), 1, 8, 0);
+            //capture a new image frame
+            captureDevice >> captureFrame;
+
+            //convert captured image to gray scale and equalize
+            cvtColor(captureFrame, grayscaleFrame, CV_BGR2GRAY);
+            equalizeHist(grayscaleFrame, grayscaleFrame);
+
+            //create a vector array to store the face found
+            std::vector<cv::Rect> faces;
+
+            //find faces and store them in the vector array
+            face_cascade.detectMultiScale(grayscaleFrame, faces, 1.1, 3, CV_HAAR_FIND_BIGGEST_OBJECT|CV_HAAR_SCALE_IMAGE, cv::Size(30,30));
+
+            //draw a rectangle for all found faces in the vector array on the original image
+            for (i = 0; i < faces.size(); i++)
+            {
+                faceDetected = true;
+                cv::Point pt1(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
+                cv::Point pt2(faces[i].x, faces[i].y);
+
+                rectangle(captureFrame, pt1, pt2, cvScalar(0, 255, 0, 0), 1, 8, 0);
+            }
+
+            //print the output
+            if (faceDetected)
+                outputVideo << captureFrame;
+
+            imshow("outputCapture", captureFrame);
+
+            if (cv::waitKey(30) >= 0)
+                break;
         }
-
-        //print the output
-        imshow("outputCapture", captureFrame);
-
-        if (cv::waitKey(30) >= 0)
-            break;
     }
 
     return 0;
 }
+
+void interruptHandler(int)
+{
+    saveVideo = false;
+}
+
